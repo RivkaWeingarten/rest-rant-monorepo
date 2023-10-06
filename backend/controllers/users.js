1921302c532e85f7524963664c6ad8df19620d1f
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-10-05 22:34:29 -0400

    Homework
    added all files

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,11 +1,33 @@
 const router = require('express').Router()
 const db = require("../models")
-
+const bcrypt = require('bcrypt')
 const { User } = db
 
 router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
+    let { password, ...rest } = req.body;
+    const user = await User.create({
+        ...rest,
+        passwordDigest: await bcrypt.hash(password,10)
+    })
+
     res.json(user)
+})
+
+router.post('/', async (req, res) => {
+    
+    let user = await User.findOne({
+        where: { 
+            email: req.body.email          
+        }
+    })
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({ 
+            message: `Could not find a user with the provided username and password` 
+        })
+    } else {
+        res.json({ user })
+    }
 })
 
 
