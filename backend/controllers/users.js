71fdb09096246b0c1b9e270662a5c5396ec0e2b2
Rivka Weingarten <126045006+RivkaWeingarten@users.noreply.github.com>
2023-10-05 23:25:45 -0400

    Update users.js
    Deleted route i added by accident

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -13,22 +13,7 @@ router.post('/', async (req, res) => {
     res.json(user)
 })
 
-router.post('/', async (req, res) => {
-    
-    let user = await User.findOne({
-        where: { 
-            email: req.body.email          
-        }
-    })
 
-    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({ 
-            message: `Could not find a user with the provided username and password` 
-        })
-    } else {
-        res.json({ user })
-    }
-})
 
 
 router.get('/', async (req, res) => {
